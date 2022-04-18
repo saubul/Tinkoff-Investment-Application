2124a8ff1991ac6eebfc5cf99e960799eb0f5295
Ruslan <miraxle@yandex.ru>
2022-04-18 20:59:26 +0300

    Инициализация проекта

---
+++ src/main/java/ru/saubulprojects/tinkoffapp/TinkoffInvestmentApplication.java
@@ -0,0 +1,13 @@
+package ru.saubulprojects.tinkoffapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkoffInvestmentApplication.class, args);
+	}
+
+}
